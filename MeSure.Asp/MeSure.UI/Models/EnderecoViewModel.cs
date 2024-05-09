495cc5774e11e4e45022e97c9794193a1fee1734
EForamiglio <erik.foramiglio.vieira@gmail.com>
2024-05-09 20:44:30 -0300

    incluindo view models

---
+++ MeSure.Asp/MeSure.UI/Models/EnderecoViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MeSure.UI.Models
+{
+    public class EnderecoViewModel : PadraoViewModel
+    {
+        public string Rua {  get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string CEP { get; set; }
+    }
+}
---
+++ MeSure.Asp/MeSure.UI/Models/SensorViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MeSure.UI.Models
+{
+    public class SensorViewModel : PadraoViewModel
+    {
+        public string FiwareId { get; set; }
+        public string FiwareName { get; set;}
+        public string SensorName { get; set;}
+        public long Temperature { get; set;}
+        public int Velocity { get; set;}
+    }
+}
